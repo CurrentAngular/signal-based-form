8143df40f88ddb8a1979af3d50f22eec8eea0eca
Valery Semenenko <gearmobile@gmail.com>
2025-10-02 19:15:02 +0300

    setup signal forms

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,25 @@
 import { Component, signal } from '@angular/core';
+import { User } from './user.interface';
+import { Control, form } from '@angular/forms/signals';
 
 @Component({
   selector: 'sbf-root',
-  imports: [],
+  imports: [Control],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  onSubmit(event: unknown): void {
+  protected readonly user = signal<User>({
+    firstName: '',
+    lastName: '',
+    email: '',
+    notify: false,
+  });
+
+  protected readonly form = form(this.user);
+
+  protected onSubmit(event: Event): void {
+    event.preventDefault();
     console.log(event);
   }
 }
