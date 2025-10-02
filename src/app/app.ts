b86d1926388ee4d390ce650be01911c3f4d267df
Valery Semenenko <gearmobile@gmail.com>
2025-10-02 19:43:15 +0300

    add validators

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { User } from './user.interface';
-import { Control, form } from '@angular/forms/signals';
+import { Control, email, form, minLength, required } from '@angular/forms/signals';
 
 @Component({
   selector: 'sbf-root',
@@ -16,7 +16,16 @@ export class App {
     notify: false,
   });
 
-  protected readonly form = form(this.user);
+  protected readonly form = form(this.user, (path) => {
+    required(path.firstName, { message: 'This field is required' });
+    minLength(path.firstName, 3, { message: 'Minimal length is 3 symbols' });
+
+    required(path.lastName, { message: 'This field is required' });
+    minLength(path.lastName, 3, { message: 'Minimal length is 3 symbols' });
+
+    required(path.email, { message: 'This field is required' });
+    email(path.email, { message: 'This is invalid email' });
+  });
 
   protected onSubmit(event: Event): void {
     event.preventDefault();
