49fe6aec409e92d8c147db1f3663d7a2d7d2b0ae
Valery Semenenko <gearmobile@gmail.com>
2025-10-26 19:32:59 +0300

    feat: initial setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { User } from './user.interface';
+import { Control, form } from '@angular/forms/signals';
 
 @Component({
   selector: 'sbf-root',
-  imports: [],
+  imports: [Control], // Control - новая директива, которая привязывает созданную модкль формы с шаблоном
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {}
+export class App {
+  // Созадли модель на основе интерфейса User
+  protected readonly user = signal<User>({
+    firstName: '',
+    lastName: '',
+    email: '',
+    notify: false,
+  });
+
+  // Используем функцию form для создания формы на основе модели
+  protected readonly userForm = form(this.user);
+
+  onSubmit(event: Event): void {
+    event.preventDefault();
+  }
+
+  constructor() {
+    // Пример, как можно обращаться к конкретному контролу формы и изменять его значение
+    // this.userForm.firstName().value.set('Gosha');
+  }
+}
