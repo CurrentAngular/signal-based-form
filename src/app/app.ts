dd0aa45976f641100d9f01f50099bff0c1cbd36a
Valery Semenenko <gearmobile@gmail.com>
2025-10-26 21:45:07 +0300

    feat: use schema
    Создали пример схемы для валидации контрола

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { User } from './user.interface';
-import { Control, email, form, minLength, required } from '@angular/forms/signals';
+import { apply, Control, email, form, minLength, required, schema } from '@angular/forms/signals';
+
+// При помощи функции schema - созадем схему, функцию, которая хранит в себе логику валидации контрола
+// преимущество такой схемы - вынесение логики валидации в отдельный скоуп и возможность переиспользования такой функции
+// <string> - говорим, что данная схема может быть применена только к контролам с типом string
+export const namesSchema = schema<string>((control) => {
+  required(control, { message: 'This field is required!' });
+  minLength(control, 2, { message: 'This field must have 2 symbols length' });
+});
 
 @Component({
   selector: 'sbf-root',
-  imports: [Control], // Control - новая директива, которая привязывает созданную модкль формы с шаблоном
+  imports: [Control], // Control - новая директива, которая привязывает созданную модель формы с шаблоном
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  // Созадли модель на основе интерфейса User
+  // Создали signal-модель на основе интерфейса User
   protected readonly user = signal<User>({
     firstName: '',
     lastName: '',
@@ -25,11 +33,9 @@ export class App {
   // ---
   // в терминологии signal form - валидация это -> field logic
   protected readonly userForm = form(this.user, (form) => {
-    required(form.firstName, { message: 'This field is required!' }); // задаем в объекте свое кастомное сообщение ошибки
-    minLength(form.firstName, 2, { message: 'This field must have 2 symbols length' }); // задаем в объекте свое кастомное сообщение ошибки
-
-    required(form.lastName, { message: 'This field is required!' }); // задаем в объекте свое кастомное сообщение ошибки
-    minLength(form.lastName, 3, { message: 'This field must have 3 symbols length' }); // задаем в объекте свое кастомное сообщение ошибки
+    // применяем созданную схему - к конкретным контролам
+    apply(form.firstName, namesSchema);
+    apply(form.lastName, namesSchema);
 
     required(form.email, { message: 'This field is required!' }); // задаем в объекте свое кастомное сообщение ошибки
     email(form.email, { message: 'This field must be email pattern' }); // задаем в объекте свое кастомное сообщение ошибки
